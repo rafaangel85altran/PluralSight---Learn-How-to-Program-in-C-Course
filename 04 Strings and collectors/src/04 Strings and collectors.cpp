998908005aac76c33bfd3013ffa239e5ff210fdb
joanrafael.angel <joanrafael.angel@28APO3050.altran.es>
2018-04-24 09:55:15 +0200

    New module Strings and collectors added

---
+++ 04 Strings and collectors/src/04 Strings and collectors.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 04.cpp
+// Author      : R Angel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "02 flowcontrol" << endl; // prints 02 flowcontrol
+	return 0;
+}
