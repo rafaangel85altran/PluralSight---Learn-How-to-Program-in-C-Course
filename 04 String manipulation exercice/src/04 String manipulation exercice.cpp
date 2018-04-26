d24333b7ea064e9d04f6aeda85efcc51092314bd
joanrafael.angel <joanrafael.angel@28APO3050.altran.es>
2018-04-26 10:08:48 +0200

    file.cpp was not importaed correctly

--- 04 String manipulation exercice/src/04 String manipulation exercice.cpp
+++ 04 String manipulation exercice/src/04 String manipulation exercice.cpp
@@ -7,9 +7,41 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main() {
-	cout << "String Manipulation exercice" << endl; // prints String Manipulation exercice
+	cout << "#####  Exercise module 4  #####" << endl; // prints String Manipulation exercise
+	cout << "Asks the user to enter two words and tells which of them  are longer" << endl;
+
+	string word1 = "", word2 = "";
+
+	cout << "Enter the first word (no spaces)" << endl;
+	//cin >> word1;
+	getline(cin,word1);
+
+	cout << "Enter the second word (no spaces)" << endl;
+	//cin >> word2;
+	getline(cin,word2);
+
+	if (word1.length() > word2.length())
+	{
+		//cout << "The first word is longer" << endl;
+		cout << "The first phrase is longer" << endl;
+	}
+	else if (word1.length() < word2.length())
+	{
+		//cout << "The second word is longer" << endl;
+		cout << "The second phrase is longer" << endl;
+	}
+	else
+	{
+		//cout << "Both words have the same length" << endl;
+		cout << "Both phrases have the same length" << endl;
+	}
+
+	system("pause");
 	return 0;
 }
+
