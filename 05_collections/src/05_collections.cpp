69a325f5c725014afffa2a2456a186b50171b31a
joanrafael.angel <joanrafael.angel@28APO3050.altran.es>
2018-04-26 10:45:29 +0200

    Module 05 added, tons of errors due to vector header and auto type. MIF

--- 05_collections/src/05_collections.cpp
+++ 05_collections/src/05_collections.cpp
@@ -7,9 +7,43 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>		//For sort and count
+
 using namespace std;
 
 int main() {
-	cout << "Collections Module" << endl; // prints Collections Module
+
+	vector<int> vi;
+	for (int i=0; i<10; i++)
+	{
+		vi.push_back(i);
+	}
+
+	for (auto item:vi)
+	{
+		cout << item << " ";
+	}
+
+	vector<string> vs;
+
+	cout << "Enter two words" << endl;
+	for (int  i=0; i<3; i++)
+	{
+		string s;
+		cin >> s;
+		vs.push_back(s);
+	}
+
+	for(auto item:vs)
+	{
+		cout << "item";
+	}
+
+	cout << endl;
+	cout << endl;
+
+	system("pause");
 	return 0;
 }
