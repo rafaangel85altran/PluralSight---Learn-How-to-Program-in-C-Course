b97d41d66ca7900de199100cc21fdc3d7e5ae557
joanrafael.angel <joanrafael.angel@28APO3050.altran.es>
2018-04-26 10:32:11 +0200

    Adding module 5_Collectors

---
+++ 05_collections/src/05_collections.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 05_collections.cpp
+// Author      : R Angel
+// Version     :
+// Copyright   : Totally free!
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Collections Module" << endl; // prints Collections Module
+	return 0;
+}
