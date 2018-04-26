0161c0927db142cfda5575042a9b708c2188bb45
joanrafael.angel <joanrafael.angel@28APO3050.altran.es>
2018-04-26 09:55:22 +0200

    Exercice "enter two words and decide tell which is longuer"

---
+++ 04 String manipulation exercice/src/04 String manipulation exercice.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 04.cpp
+// Author      : R Angel
+// Version     :
+// Copyright   : Totally free!
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "String Manipulation exercice" << endl; // prints String Manipulation exercice
+	return 0;
+}
