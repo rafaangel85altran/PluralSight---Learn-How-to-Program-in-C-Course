7e37229aecf6eae8a92eff7a00d49605c6e2b9aa
win7 <win7@win7-PC>
2018-05-15 20:46:58 +0200

    New project encapsulation

---
+++ 06_writting classes_encapsulation/src/06_writting classes_encapsulation.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 06_writting.cpp
+// Author      : RA
+// Version     :
+// Copyright   : Just for training
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Module 06 Writting classes - Encapsulation" << endl; // prints Module 06 Writting classes - Encapsulation
+	return 0;
+}
