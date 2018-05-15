5c23f6221db68b45fe4c253e75310044a69c9852
win7 <win7@win7-PC>
2018-05-15 09:10:58 +0200

    Add module 06 writting classes

---
+++ 06_Writting_classes/src/06_Writting_classes.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : 06_Writting_classes.cpp
+// Author      : RA
+// Version     :
+// Copyright   : Just for training
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Module 06 Writting classes" << endl; // prints Module 06 Writting classes
+	return 0;
+
+}
