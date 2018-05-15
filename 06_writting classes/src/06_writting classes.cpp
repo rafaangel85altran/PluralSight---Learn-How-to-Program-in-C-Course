f9d997fc2ac8a233d3da785958ce56350671e005
win7 <win7@win7-PC>
2018-05-15 18:15:14 +0200

    Some edit to thecode inorder to see what happens if the withdraw is OK

--- 06_writting classes/src/06_writting classes.cpp
+++ 06_writting classes/src/06_writting classes.cpp
@@ -14,24 +14,28 @@ using namespace std;
 int main() {
 
 	Account a1;									//An account object called a1
-	a1.Deposit(90);
+	a1.Deposit(90);								//Deposit of $90
 
 	cout << "After despositting $90" << endl;
 	for (auto s:a1.Report())					//Ranged for loop seen in Module 05 collectors
 	{
-		cout << s << endl;
+		cout << s << endl;						//This is the cout used to print out the report
 	}
 
-	a1.Withdraw(50);
+	a1.Withdraw(50);							//Called withdraw memeber function of Account a1 object
 
-	if (a1.Withdraw(100))						//If the withdraw is true
+	if (a1.Withdraw(10))						//If the withdraw is true
 	{
 		cout << "Second withdraw success" << endl;
 	}
+	else
+	{
+		cout << "Withdraw not possible, not enough money, pal" << endl;
+	}
 
-	cout << "After withdraw $50 then $100" << endl;
+	cout << "After withdraw $50 then $10" << endl;
 
-	for (auto s:a1.Report())
+	for (auto s:a1.Report())					//Final report
 	{
 		cout << s << endl;
 	}
