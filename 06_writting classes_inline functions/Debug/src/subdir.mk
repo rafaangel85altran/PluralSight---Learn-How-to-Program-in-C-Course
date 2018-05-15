76c69a52e66e54d6b631e790922534f3d371dee6
win7 <win7@win7-PC>
2018-05-15 20:07:10 +0200

    Added inline section for the module 06 Writting classes

---
+++ 06_writting classes_inline functions/Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/06_writting\ classes_inline\ functions.cpp 
+
+OBJS += \
+./src/06_writting\ classes_inline\ functions.o 
+
+CPP_DEPS += \
+./src/06_writting\ classes_inline\ functions.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/06_writting\ classes_inline\ functions.o: ../src/06_writting\ classes_inline\ functions.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/06_writting classes_inline functions.d" -MT"src/06_writting\ classes_inline\ functions.d" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ 06_writting classes_inline functions/src/06_writting classes_inline functions.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 06_writting.cpp
+// Author      : RA
+// Version     :
+// Copyright   : Just for training
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Module 06 Writting classes - In line section" << endl; // prints Module 06 Writting classes - In line section
+	return 0;
+}
