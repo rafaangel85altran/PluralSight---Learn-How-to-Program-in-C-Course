b7fdc29ad9d1ddff8e1ca1e0669c1f84c5b980b8
win7 <win7@win7-PC>
2018-05-16 06:08:45 +0200

    casting example performed, no big deal

--- 08_Casting/src/08_Casting.cpp
+++ 08_Casting/src/08_Casting.cpp
@@ -10,6 +10,28 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl; // prints Hello World!
+	cout << "Hello World!" << endl << 2 + 2;
+
+	cout << endl << endl;
+
+	int i;
+	//Fractional part will be lost
+
+	i= static_cast<int> (4.9);			//Explicit casting, warning will not be shown
+	cout << i << endl;
+
+	cout << i << endl;
+
+	auto j=5;
+	cout << j << endl;
+
+	auto f = 4.9;
+	j=static_cast<int> (f);				//Explicit casting, warning will not be shown
+
+	cout << f << endl;
+	f = 9.0/5;
+	cout << f << endl;
+
+	system("pause");
 	return 0;
 }
