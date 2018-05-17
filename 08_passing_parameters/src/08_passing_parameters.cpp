951753b81dc5908334f22eb0bf615ea5aa3bd0a4
win7 <win7@win7-PC>
2018-05-17 19:34:32 +0200

    passing parameters project copy

---
+++ 08_passing_parameters/src/08_passing_parameters.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 08_passing_parameters.cpp
+// Author      : RA
+// Version     :
+// Copyright   : Just for training
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
